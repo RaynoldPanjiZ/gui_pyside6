3604449c8360553475c422fa38439e064cf9acde
RaynoldPanjiZ <raynoldpanji922@gmail.com>
2024-08-26 20:35:32 +0700

    update10

--- test_opencv.py
+++ test_opencv.py
@@ -1,10 +1,11 @@
 import cv2
 
 
-# url = "vid/test1.mp4"
-url = "rtsp://admin:aery2021!@192.168.45.166:554/cam/realmonitor?channel=1&subtype=0&unaicast=true&proto=Onvif"
+url = "vid/test1.mp4"
+# url = "rtsp://admin:aery2021!@192.168.45.166:554/cam/realmonitor?channel=1&subtype=0&unaicast=true&proto=Onvif"
 cap = cv2.VideoCapture(url)
 
+i = 0
 while True:
     ret, frame = cap.read()
     print(frame.shape)
@@ -14,3 +15,8 @@ while True:
     # if key == 27:
     #     break
     # cv2.destroyAllWindows()
+    if i>10:
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        cv2.imwrite("./test.jpg", frame)
+        break
+    i+=1
